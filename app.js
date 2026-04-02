98fba8435c868ce083762c8e3dfcd4a160b402a4
NarmeenSheeraz <sheeraznarmeen@gmail.com>
2026-04-02 20:42:17 +0500

    update html css and js

--- app.js
+++ app.js
@@ -1,139 +1,144 @@
 var questions = [
   {
     question: "What is the correct syntax to declare a variable in JavaScript?",
-    options: ["var variableName;", "let variableName;", "const variableName;", "All of the above"],
+    opt1 : "var variableName;",
+    opt2: "let variableName;",
+    opt3: "const variableName;",
+    opt4: "All of the above",
     ans: "All of the above"
   },
   {
     question: "Which of the following is used to create an object in JavaScript?",
-    options: ["{}", "[]", "()", "< >"],
+     opt1 : "{}",
+    opt2: "[]",
+    opt3: "()",
+    opt4: "<>",
     ans: "{}"
   },
   {
     question: "Which method is used to parse a string to an integer in JavaScript?",
-    options: ["parseInt()", "parseInteger()", "parseNumber()", "parseFloat()"],
+     opt1 : "parseInt()",
+    opt2: "parseInteger()",
+    opt3: "parseNumber()",
+    opt4: "parseFloat()",
     ans: "parseInt()"
   },
   {
     question: "Which keyword is used to define a constant in JavaScript?",
-    options: ["var", "let", "const", "define"],
+    opt1 : "var",
+    opt2: "let",
+    opt3: "const",
+    opt4: "define",
     ans: "const"
   },
-  {
-    question: "Which company developed JavaScript?",
-    options: ["Microsoft", "Netscape", "Google", "Apple"],
-    ans: "Netscape"
-  },
-  {
-    question: "Which symbol is used for comments in JavaScript (single-line)?",
-    options: ["//", "<!-- -->", "/* */", "#"],
-    ans: "//"
-  },
-  {
-    question: "How do you write 'Hello' in an alert box?",
-    options: ["alert(Hello)", "msg(Hello)", "alert('Hello')", "console.log(Hello)"],
-    ans: "alert('Hello')"
-  },
-  {
-    question: "Which operator is used to assign a value to a variable?",
-    options: ["*", "=", "-", "+"],
-    ans: "="
-  },
-  {
-    question: "What does DOM stand for?",
-    options: ["Document Object Model", "Data Object Model", "Digital Ordinance Model", "Desktop Object Management"],
-    ans: "Document Object Model"
-  },
-  {
-    question: "Which method is used to select an element by ID?",
-    options: ["getElementById()", "querySelectorAll()", "getElementsByClassName()", "selectById()"],
-    ans: "getElementById()"
-  }
+  // {
+  //   question: "Which company developed JavaScript?",
+  //   options: ["Microsoft", "Netscape", "Google", "Apple"],
+  //   ans: "Netscape"
+  // },
+  // {
+  //   question: "Which symbol is used for comments in JavaScript (single-line)?",
+  //   options: ["//", "<!-- -->", "/* */", "#"],
+  //   ans: "//"
+  // },
+  // {
+  //   question: "How do you write 'Hello' in an alert box?",
+  //   options: ["alert(Hello)", "msg(Hello)", "alert('Hello')", "console.log(Hello)"],
+  //   ans: "alert('Hello')"
+  // },
+  // {
+  //   question: "Which operator is used to assign a value to a variable?",
+  //   options: ["*", "=", "-", "+"],
+  //   ans: "="
+  // },
+  // {
+  //   question: "What does DOM stand for?",
+  //   options: ["Document Object Model", "Data Object Model", "Digital Ordinance Model", "Desktop Object Management"],
+  //   ans: "Document Object Model"
+  // },
+  // {
+  //   question: "Which method is used to select an element by ID?",
+  //   options: ["getElementById()", "querySelectorAll()", "getElementsByClassName()", "selectById()"],
+  //   ans: "getElementById()"
+  // }
 ];
-
 var index = 0;
 var score = 0;
-var timeLeft = 30;
-var timer;
-
-function startTimer() {
-  clearInterval(timer);
-  timeLeft = 30;
-
-  timer = setInterval(() => {
-    timeLeft--;
-    document.getElementById("time").innerText = timeLeft + "s";
-
-    if (timeLeft <= 0) {
-      clearInterval(timer);
-      next(); 
+function renderQuiz(){
+  var quizcontainer = document.getElementById("quizcontainer")
+  var option = document.getElementsByName("option")
+  for(var i = 0; i < option.length; i++){
+    if(option[i].checked){
+      if(option[i].value == questions[index-1].ans){
+        score++
+        
+      }
     }
-  }, 3000);
-}
-
-function render() {
-  var app = document.getElementById("app");
-
-  if (index >= questions.length) {
-    clearInterval(timer);
-    var percent = ((score / questions.length) * 100).toFixed(0);
-    app.innerHTML = `
-      <div class="result-box">
-        <h2>🎉 Quiz Completed</h2>
-        <h3>${percent}% Score</h3>
-        <p>You got ${score} out of ${questions.length} correct</p>
-        <button class="btn btn-primary" onclick="restart()">Restart</button>
+  }
+  if(!questions[index]){
+    quizcontainer.innerHTML = `<h1>${score}</h1>`
+    return
+  }
+  
+  quizcontainer.innerHTML = `
+    <div class="bg-light timer d-flex align-items-center justify-content-center mx-auto"><h3 class="text-center mt-1" id="timer">30</h3></div>
+      <p class="fw-bold fs-5 card p-4 mt-2">${questions[index].question}</p>
+      <div>
+        <div class="card p-1 mt-4 indiv ">
+          <label class="mt-2"><input type="radio" name="option" value="${questions[index].opt1}" class="mb-3 ms-3">
+            ${questions[index].opt1}</label>
+        </div>
+        <div class="card p-1 my-3 indiv">
+          <label class="mt-2"><input type="radio" name="option" value="${questions[index].opt2}" class="mb-3 ms-3">
+            ${questions[index].opt2}</label>
+        </div>
+        <div class="card p-1 indiv">
+          <label class="mt-2"><input type="radio" name="option" value="${questions[index].opt3}" class="mb-3 ms-3">
+            ${questions[index].opt3}</label>
+        </div>
+        <div class="card p-1 my-3 indiv">
+          <label class="mt-2"><input type="radio" name="option" value="${questions[index].opt4}" class="mb-3 ms-3">
+            ${questions[index].opt4}</label>
+        </div>
+        <div class="d-flex ">
+          <button class="btn btn-danger ms-auto" onclick="previous()">Previous</button>
+          <button class="btn btn-success  ms-4 " onclick="next()">Next</button>
+        </div>
       </div>
-    `;
-    return;
-  }
 
-  var q = questions[index];
+  `
+  
+  index++
+  timer()
+}
+function next(){
 
-  app.innerHTML = `
-    <div class="timer">⏱ Time Left: <span id="time">30s</span></div>
-
-    
-
-    <div class="question">Q${index + 1}: ${q.question}</div>
-
-    ${q.options.map(opt => `
-      <label class="option">
-        <input type="radio" name="option" value="${opt}"> ${opt}
-      </label>
-    `).join("")}
-
-    <div class="d-flex justify-content-between mt-3">
-      <button class="btn btn-secondary btn-custom" onclick="prev()" ${index==0?'disabled':''}>Previous</button>
-      <button class="btn btn-success btn-custom" onclick="next()">${index==questions.length-1?'Submit':'Next'}</button>
-    </div>
-  `;
-
-  startTimer();
 }
 
-function next() {
-  var selected = document.querySelector('input[name="option"]:checked');
+renderQuiz()
 
-  if (selected && selected.value === questions[index].ans) {
-    score++;
-  }
 
-  index++;
-  render();
+function timer(){
+  var time = 30; 
+  var interval = setInterval(()=>{
+   var timeid = document.getElementById("timer")
+   timeid.innerHTML = time 
+   time--
+   if(time < 0 ){
+     clearInterval(interval)
+     renderQuiz()
+   }
+  },1000)
 }
-
-function prev() {
-  if (index > 0) {
-    index--;
-    render();
+function submit(){
+  var submit = document.getElementsByClassName("enName")[0]
+  var suba = document.getElementById("submit")
+  
+  if(submit.value.trim()){
+    var suba = document.getElementById("submit")
+    suba.setAttribute("href", "quiz.html")
   }
 }
 
-function restart() {
-  index = 0;
-  score = 0;
-  render();
-}
 
-render();
+
